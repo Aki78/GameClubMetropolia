44c90539c08abedba466c4875433e29eae11effc
DeK0n <dv.knnv@gmail.com>
2022-12-22 14:47:37 +0200

    ex12

--- exercises/Exercise12.gd
+++ exercises/Exercise12.gd
@@ -4,17 +4,29 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
- var tweenNode = get_node("Icon/Tween")
+onready var tweenNode = get_node("Icon/Tween")
+var distance = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func icon_Tween():
+	distance = ($Icon.position.distance_to(get_global_mouse_position()))/50
+	tweenNode.interpolate_property($Icon,"position",$Icon.position,get_global_mouse_position(), 1*distance*0.0167, tweenNode.TRANS_LINEAR)
+	tweenNode.start()	
 
 func _on_Button_pressed():
-	pass # Replace with function body.
+	$Button.disabled = true
+	icon_Tween()
+	yield(tweenNode,"tween_completed")
+	$Button.disabled = false
+	print(distance)
+	print("pressed")
+	print($Icon.position)
+	print(get_global_mouse_position())
+	
